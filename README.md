939b8196d783a2f5924d233a658b02d1df49a3c1
Hans Jespersen <hans.jespersen@gmail.com>
2014-10-12 13:17:22 -0700

    first try

---
+++ README.md
@@ -0,0 +1,41 @@
+node-red-contrib-teslams
+========================
+
+Node-Red (http://nodered.org) nodes for communicating with a Tesla Model S electric car. 
+Based on the teslams javascript libraries (https://github.com/hjespers/teslams).
+
+These programs and documentation do not come from Tesla Motors Inc.
+
+Be careful when using these programs as they can lock and unlock your car as well as control various functions relating to the charging system, sun roof, headlights, horn, climate control, and other subsystems of the car.
+
+Be careful not to send your login and password to anyone other than Tesla or you are giving away the authentication details required to control your car.
+
+Also ensure that you don't overwhelm the Tesla servers with requests. Calling functions at very high frequency can put substantial load on the Tesla servers and might get your IP blocked by Tesla. Tesla might also reset your login for too many failed login attempts.
+
+
+#Install
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-teslams
+
+
+#Usage
+
+You will need a Tesla Model S. The ultimate "thing" to connect to the Internet of Things.
+You will need an owners account on the Telsa Motors Inc. official web site (http://www.teslamotors.com) associated with your car(s).
+Execute the "login" node at least once to get the appropriate security tokens for all the other nodes.
+All nodes besides the "login" node require the vehicle "id" to be in the input msg.payload. 
+
+
+#Disclaimer
+
+Use these programs at your own risk. The author does not guaranteed the proper functioning of these applications. This code attempts to use the same interfaces used by the official Tesla phone apps. However, it is possible that use of this code may cause unexpected damage for which nobody but you are responsible. Use of these functions can change the settings on your car and may have negative consequences such as (but not limited to) unlocking the doors, opening the sun roof, or reducing the available charge in the battery.
+
+# Author
+
+Hans Jespersen, https://github.com/hjespers
+
+#Feedback and Support
+
+For more information, feedback, or community support see the Tesla Motors Club forum at http://www.teslamotorsclub.com/showthread.php/20325-TeslaMS-tools-for-telemetry-data-visualization or email teslams@googlegroups.com
---
+++ teslams/teslams.js
@@ -0,0 +1,420 @@
+// By Hans Jespersen
+
+module.exports = function(RED) {
+    "use strict";
+    var teslams = require('teslams');
+
+    function TeslaLogin(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var tesla_email = this.credentials.user;
+        var tesla_password = this.credentials.password;
+
+        this.on("input", function(msg) {
+            //console.log( 'teslams called with message: ' + util.inspect(msg) );
+            //console.log( 'this looks like: ' + util.inspect(this) );
+            //console.log( 'n looks like: ' + util.inspect(n) );
+
+            //console.log( 'attempting login with email = ' + tesla_email + ' andd password: ' + tesla_password);
+            
+            var outmsg = { 
+                topic: msg.topic
+            };
+            try{   
+                teslams.all( { email: tesla_email, password: tesla_password }, function ( err, res, body ) {
+                    //console.log('tesla sucess: ' + util.inspect(body));
+                    outmsg.payload = body;
+                    node.send(outmsg);
+                });
+            } catch (e) {
+                console.log('Telsa login error: ' + e);
+                outmsg.payload = e;
+                node.send(outmsg);
+            }
+        });
+    }
+    RED.nodes.registerType("login",TeslaLogin,{
+        credentials: {
+            user: {type:"text"},
+            password: {type: "password"}
+        }
+    });
+
+    function TeslaCmd(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var commandType = n.command;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            //console.log( 'tesla command node called with msg: ' + util.inspect(msg) );
+            //console.log( 'tesla command type: ' + commandType );
+            //console.log( 'this looks like: ' + util.inspect(this) );
+            //console.log( 'n looks like: ' + util.inspect(n) );
+
+            var vehicles = msg.payload;
+            //console.log( 'tesla command node called with vehicles: ' + vehicles );
+            if ( vehicles instanceof Array) {
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                //console.log( 'tesla command node called with vehicle: ' + vehicle );
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla command node got incorrect vehicle array on input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+            //console.log( 'tesla command node called with vid: ' + vid );
+
+            if (vid) {
+                switch(commandType) {
+                    case "flash":
+                        teslams.flash( vid, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);
+                        });
+                        break;
+                    case "honk":
+                        teslams.honk( vid, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);
+                        });
+                        break;
+                    case "get_drive_state":
+                        teslams.get_drive_state( vid, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);                       
+                        });
+                        break;
+                    case "get_vehicle_state":
+                        teslams.get_vehicle_state( vid, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);                       
+                        });
+                        break;
+                    case "get_climate_state":
+                        teslams.get_climate_state( vid, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);                   
+                        });
+                        break;
+                    case "get_charge_state":
+                        teslams.get_charge_state( vid, function ( resp ) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);     
+                        });
+                        break;
+                    case "get_gui_settings":
+                        teslams.get_gui_settings( vid, function ( resp ) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);     
+                        });
+                        break;
+                    case "open_charge_port":
+                        teslams.open_charge_port( vid, function ( resp ) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);     
+                        });
+                        break;
+                    case "wake_up":
+                        teslams.wake_up( vid, function ( resp ) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);     
+                        });
+                        break;
+                    default:
+                        console.log('undefined commandType');
+                }                        
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+            }
+        });
+    }
+    RED.nodes.registerType("command",TeslaCmd);
+
+    function TeslaLock(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var state = n.state;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            //console.log( 'tesla lock node called with msg: ' + util.inspect(msg) );
+            //console.log( 'tesla lock type: ' + commandType );
+            //console.log( 'this looks like: ' + util.inspect(this) );
+            //console.log( 'n looks like: ' + util.inspect(n) );
+
+            var vehicles = msg.payload;
+            //console.log( 'tesla command node called with vehicles: ' + vehicles );
+            if ( vehicles instanceof Array) {
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                //console.log( 'tesla command node called with vehicle: ' + vehicle );
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla command node called with unexpected input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+            //console.log( 'tesla command node called with vid: ' + vid );
+
+            if (vid) {
+                switch(state) {
+                    case "lock":
+                        teslams.door_lock( {id: vid, lock: "lock" }, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);
+                        });
+                        break;
+                    case "unlock":
+                        teslams.door_lock( {id: vid, lock: "unlock"}, function (resp) {
+                            outmsg.payload = resp;
+                            node.send(outmsg);
+                        });
+                        break;
+                    default:
+                        console.log('undefined lock state');
+                }                        
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+            }
+        });
+    }
+    RED.nodes.registerType("lock",TeslaLock);
+  
+    function TeslaSunroof(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var state = n.state;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            var err = null;
+
+            var vehicles = msg.payload;
+            if ( vehicles instanceof Array) {          
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla sunroof node called with unexpected input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+
+            if (vid) {
+                if (state == "open" || state == "close" || state == "comfort" || state == "vent") {
+                    teslams.sun_roof( {id: vid, roof: state }, function (resp) {
+                        outmsg.payload = resp;
+                        node.send(outmsg);
+                    });
+                } else {
+                    err = new Error("Invalid sun roof state. Specify 'open', 'close', 'comfort' or 'vent'");
+                    outmsg.payload = err;
+                    node.send(outmsg);         
+                }
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+                err = new Error("undefined vehicle id, input must be properly formated tesla vehicle array with 'id' field of car set");
+                outmsg.payload = err;
+                node.send(outmsg);  
+            }
+        });
+    }
+    RED.nodes.registerType("sunroof",TeslaSunroof);
+
+    function TeslaCharger(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var state = n.state;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            var err = null;
+
+            var vehicles = msg.payload;
+            if ( vehicles instanceof Array) {          
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla charger node called with unexpected input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+
+            if (vid) {
+                if (state == "start" || state == "stop" ) {
+                    teslams.charge_state( { id: vid, charge: state }, function (resp) {
+                        outmsg.payload = resp;
+                        node.send(outmsg);
+                    });
+                } else {
+                    err = new Error("Invalid charge state. Use 'start' or 'stop'");
+                    outmsg.payload = err;
+                    node.send(outmsg);         
+                }
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+                err = new Error("undefined vehicle id, input must be properly formated tesla vehicle array with 'id' field of car set");
+                outmsg.payload = err;
+                node.send(outmsg);  
+            }
+        });
+    }
+    RED.nodes.registerType("charger",TeslaCharger);
+
+    function TeslaAutoConditioning(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var state = n.state;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            var err = null;
+
+            var vehicles = msg.payload;
+            if ( vehicles instanceof Array) {          
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla auto conditioning node called with unexpected input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+
+            if (vid) {
+                if (state == "start" || state == "stop" ) {
+                    teslams.auto_conditioning( { id: vid, climate: state}, function (resp) {
+                        outmsg.payload = resp;
+                        node.send(outmsg);
+                    });
+                } else {
+                    err = new Error("Invalid auto conditioning state. Use 'start' or 'stop'");
+                    outmsg.payload = err;
+                    node.send(outmsg);         
+                }
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+                err = new Error("undefined vehicle id, input must be properly formated tesla vehicle array with 'id' field of car set");
+                outmsg.payload = err;
+                node.send(outmsg);  
+            }
+        });
+    }
+    RED.nodes.registerType("climate",TeslaAutoConditioning);
+
+    function TeslaChargeRange(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var range = n.range;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            var err = null;
+
+            var vehicles = msg.payload;
+            if ( vehicles instanceof Array) {          
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla charge range node called with unexpected input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+
+            if (vid) {
+                if ( range >= 50 && range <= 100 ) {
+                    teslams.charge_range( { id: vid, range: 'set', percent: range }, function (resp) {
+                        outmsg.payload = resp;
+                        node.send(outmsg);
+                    });
+                } else {
+                    err = new Error("Invalid charge range. Use a number from between 50-100 percent");
+                    outmsg.payload = err;
+                    node.send(outmsg);         
+                }
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+                err = new Error("undefined vehicle id, input must be properly formated tesla vehicle array with 'id' field of car set");
+                outmsg.payload = err;
+                node.send(outmsg);  
+            }
+        });
+    }
+    RED.nodes.registerType("charge-range",TeslaChargeRange);
+
+    function TeslaSetTemperature(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        var temp = n.temp;
+
+        this.on("input", function(msg) {
+            var outmsg = {
+                topic: msg.topic
+            };
+            var vid = null;
+            var err = null;
+
+            var vehicles = msg.payload;
+            if ( vehicles instanceof Array) {          
+                var vehicle = vehicles[0]; //first car in the vehicles array
+                if(vehicle.id) {
+                    vid = vehicle.id; // the three digit vehicle "id"
+                }
+            } else {
+                console.log( 'tesla temp range node called with unexpected input: ' + msg.payload);
+                outmsg.payload = msg.payload;
+                node.send(outmsg);
+            }
+
+            if (vid) {
+                if ( temp >= 17 && temp <= 32 ) {
+                    console.log('setting temp to ' + temp);
+                    teslams.set_temperature( { id: vid, dtemp: temp}, function (resp) {
+                        outmsg.payload = resp;
+                        node.send(outmsg);
+                    });
+                } else {
+                    err = new Error("Invalid temp value. Valid range is 17C - 32C");
+                    outmsg.payload = err;
+                    node.send(outmsg);         
+                }
+            } else {
+                console.log('undefined vid, skipping calls to tesla');
+                err = new Error("Invalid temp value. Valid range is 17C - 32C" );
+                outmsg.payload = err;
+                node.send(outmsg);  
+            }
+        });
+    }
+    RED.nodes.registerType("set-temp",TeslaSetTemperature);
+
+};
+
